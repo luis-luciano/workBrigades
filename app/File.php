3c53072fd44f9efc55d5a9104fd3cdfe198ce48c
luis-luciano <alberto_rammss@hotmail.com>
2016-07-19 19:39:20 -0500

    iniciacion de modelo File para archivos

--- app/File.php
+++ app/File.php
@@ -2,9 +2,29 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+
+	public function owner()
+	{
+		return $this->belongsTo('App\User','user_id');
+	}
+
+    public function getExtensionAttribute()
+    {
+    	return pathinfo($this->name,PATHINFO_EXTENSION);
+    }
+
+    public function getTypeAttribute()
+    {
+    	if(in_array($this->extension,['jpeg', 'jpg', 'png'])){
+    		return 'image';
+    	}
+
+    	return 'document';
+    }
 }
