12d8554662fcd67d725b58b0e2b43c9cea7e538e
ivanDelaluzA <ivan.delaluz.93@gmail.com>
2016-07-27 17:33:05 -0500

    Funcionalidad a las graficas

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,21 +13,14 @@ class PagesController extends Controller
     public function index()
     {
 
-    	$r=Petition::countTypology(3)->count();
-
-
-    	
-
-    	
-
-
-    	$typologies=Typology::all();
-
-    	$counters=array();
-    	foreach ($typologies as $typology) {
-    		$counters[$typology->id]=Petition::countTypology($typology->id)->count();
-    	}
-    	$requests=Petition::all();
+        $r=Petition::countTypology(3)->count();
+        $typologies=Typology::all();
+        $counters=array();
+        foreach ($typologies as $typology) {
+            $counters[$typology->id]=Petition::countTypology($typology->id)->count();
+        }
+        $requests=Petition::all();
+        $counters=json_encode($counters);
         return view('pages.index',compact('typologies','requests','counters'));
     }
 
