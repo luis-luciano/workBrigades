30d1d66985fba883efab48df7a0b6f3ebd0404b6
unknown <parak_93@hotmail.com>
2016-07-29 10:19:43 -0500

    table requets

--- resources/views/partials/requests/table.blade.php
+++ resources/views/partials/requests/table.blade.php
@@ -1,50 +1,52 @@
-<table id="dataTable" class="table my-table-striped table-ellipsis">
-    <thead class="bg-blue-grey text-white">
-        <tr>
-           <th class="col-md-1">Folio</th>
-           <th class="col-md-2">Estado</th>
-           <th class="col-md-2">Problema</th>
-           <th class="col-md-1">Prioridad</th>
-           <th class="col-md-2">Ciudadano</th>
-           <th class="col-md-4">Domicilio</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($requests as $request)
-            <tr class='clickable-row' data-href='{{ route($baseRequestRoute, $request) }}'>
-                 <td>
-                                    {{ $request->id }}
-                                </td>
-                           {{--  <input type="hidden" id="_url" value="{{ action('RequestsController@edit',$request->id)}}"></a>  --}}
-                                <td id="state">
-                                    <div class="status text-white text-center" style="background: {{ $request->state->color }}; border-radius: 10px">
-                                        {{ $request->state->label }}
-                                    </div>
-                                </td> 
-                                <td>
-                                    {{ $request->problem->name }}
-                                </td>    
-                                <td>
-                                    <div class="status text-white text-center" style="background: {{ $request->priority->color }}; border-radius: 10px">
-                                        {{ $request->priority->name }}
-                                    </div>
-                                </td>              
-                                <td>
-                                    {{ $request->concerned->full_name}}          
-                                </td>  
-                                <td>
-                                    {{ $request->colony->settlementType->name }}: {{ $request->colony->name }} <br>
-                                    Calle: {{ $request->street }} 
-                                    {{  (!empty($request->number)) ? "Número: " . $request->number : "" }}
-                                    {{  (!empty($request->reference)) ? "Referencia: " . $request->reference : "" }}
-                                </td>  
+<div class="table-responsive">
+    <table id="dataTable" class="table table-striped table-bordered table-hover table-condensed clickable-rows">
+        <thead>
+            <tr>
+               <th class="col-md-1">Folio</th>
+               <th class="col-md-2">Estado</th>
+               <th class="col-md-2">Problema</th>
+               <th class="col-md-1">Prioridad</th>
+               <th class="col-md-2">Ciudadano</th>
+               <th class="col-md-4">Domicilio</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
-<div class="pull-left">
-    {{ $requests->total() }} {{ plural('requests') }}
-</div>
-<div class="pull-right">
-    {!! $requests->render() !!}
-</div>
+        </thead>
+        <tbody>
+            @foreach ($requests as $request)
+                <tr class='clickable-row' data-href='{{ route($baseRequestRoute, $request) }}'>
+                     <td>
+                                        {{ $request->id }}
+                                    </td>
+                               {{--  <input type="hidden" id="_url" value="{{ action('RequestsController@edit',$request->id)}}"></a>  --}}
+                                    <td id="state">
+                                        <div class="status text-white text-center" style="background: {{ $request->state->color }}; border-radius: 10px">
+                                            {{ $request->state->label }}
+                                        </div>
+                                    </td> 
+                                    <td>
+                                        {{ $request->problem->name }}
+                                    </td>    
+                                    <td>
+                                        <div class="status text-white text-center" style="background: {{ $request->priority->color }}; border-radius: 10px">
+                                            {{ $request->priority->name }}
+                                        </div>
+                                    </td>              
+                                    <td>
+                                        {{ $request->concerned->full_name}}          
+                                    </td>  
+                                    <td>
+                                        {{ $request->colony->settlementType->name }}: {{ $request->colony->name }} <br>
+                                        Calle: {{ $request->street }} 
+                                        {{  (!empty($request->number)) ? "Número: " . $request->number : "" }}
+                                        {{  (!empty($request->reference)) ? "Referencia: " . $request->reference : "" }}
+                                    </td>  
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <div class="pull-left">
+        {{ $requests->total() }} {{ plural('requests') }}
+    </div>
+    <div class="pull-right">
+        {!! $requests->render() !!}
+    </div>
+</div> {{-- table responsive --}}
