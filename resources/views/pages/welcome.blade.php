4804a47e2315b4025ebae0800aaf7a29709da256
unknown <parak_93@hotmail.com>
2016-08-12 10:07:16 -0500

    view welcome and create logos

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -20,6 +20,142 @@
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<script src="{{ asset('assets/globals/js/global-vendors.js') }}"></script>
+	<style type="text/css">
+		.btn-personality-b{
+
+			background: rgba(20, 145, 183, 0.64); 
+		}
+
+		.btn-personality-b:hover{
+			   /* background: rgba(20, 145, 183, 0.79);*/
+
+
+			background: rgba(242, 242, 242, 0.47);
+			color: #000000;
+		}
+
+		.ch-grid {
+			margin: 20px 0 0 0;
+			padding: 0;
+			list-style: none;
+			display: block;
+			text-align: center;
+			width: 100%;
+		}
+
+		.ch-grid:after,
+		.ch-item:before {
+			content: '';
+		    display: table;
+		}
+
+		.ch-grid:after {
+			clear: both;
+		}
+
+		.ch-grid li {
+			width: 220px;
+			height: 220px;
+			display: inline-block;
+			margin: 20px;
+		}
+
+		.ch-item {
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			overflow: hidden;
+			position: relative;
+			cursor: default;
+			box-shadow: 
+				inset 0 0 0 16px rgba(255,255,255,0.6),
+				0 1px 2px rgba(0,0,0,0.1);
+			transition: all 0.4s ease-in-out;
+		}
+
+		.ch-img-1 { 
+			background-image: url({{ asset('assets/globals/img/resources/requ.png') }});
+			opacity: 0.7;
+		}
+
+		.ch-img-2 { 
+			background-image: url({{ asset('assets/globals/img/resources/buscar.png') }});
+			opacity: 0.7;
+		}
+
+		.ch-img-3 { 
+			background-image: url({{ asset('assets/globals/img/resources/requ.png') }});
+		}
+
+		.ch-info {
+			position: absolute;
+			background: rgba(63,147,147, 0.8);
+			width: inherit;
+			height: inherit;
+			border-radius: 50%;
+			overflow: hidden;
+			opacity: 0;
+			transition: all 0.4s ease-in-out;
+			transform: scale(0);
+		}
+
+		.ch-info h3 {
+			color: #fff;
+			text-transform: uppercase;
+			letter-spacing: 2px;
+			font-size: 22px;
+			margin: 0 30px;
+			padding: 45px 0 0 0;
+			height: 140px;
+			font-family: 'Open Sans', Arial, sans-serif;
+			text-shadow: 
+				0 0 1px #fff, 
+				0 1px 2px rgba(0,0,0,0.3);
+		}
+
+		.ch-info p {
+			color: #fff;
+			padding: 10px 5px;
+			font-style: italic;
+			margin: 0 30px;
+			font-size: 12px;
+			border-top: 1px solid rgba(255,255,255,0.5);
+			opacity: 0;
+			transition: all 1s ease-in-out 0.4s;
+		}
+
+		.ch-info p a {
+			display: block;
+			color: rgba(255,255,255,0.7);
+			font-style: normal;
+			font-weight: 700;
+			text-transform: uppercase;
+			font-size: 9px;
+			letter-spacing: 1px;
+			padding-top: 4px;
+			font-family: 'Open Sans', Arial, sans-serif;
+		}
+
+		.ch-info p a:hover {
+			color: rgba(255,242,34, 0.8);
+		}
+
+		.ch-item:hover {
+			box-shadow: 
+				inset 0 0 0 1px rgba(255,255,255,0.1),
+				0 1px 2px rgba(0,0,0,0.1);
+		}
+
+		.ch-item:hover .ch-info {
+			transform: scale(1);
+			opacity: 1;
+		}
+		
+		.ch-item:hover .ch-info p {
+			opacity: 1;
+		}
+
+	</style>
 </head>
 
 <body class="demo-1">
@@ -36,11 +172,16 @@
 
 	</div>
 	<div class="">
-
+	
 		<header class="codrops-header">
 			
 			<img class="img-logo" src="{{ asset('assets/globals/img/logo_h.png') }}" />
-
+			<a href="">
+			 	<button type="button" class="btn btn-personality-b">Ver video
+			 		{{-- <span class="" style=""></span>
+			 		<span class="ripple _7 animate" style=""></span> --}}
+			 	</button>
+	  		</a>
 		</header>
 
 		<div class="slideshow">
@@ -49,42 +190,53 @@
 			</canvas>
 			<!-- Heavy Rain -->
 			<div class="slide" id="slide-1" data-weather="rain">
-				<!-- Form Module-->
+				<ul class="ch-grid">
+					<li>
+						<div class="ch-item ch-img-1">
+							<div class="ch-info">
+								<h3>N U E V A</h3>
+								<p>P E T I C I Ó N<a href="Peticion-publica">Crear Petición</a></p>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-2">
+							<div class="ch-info">
+								<h3>S I S T E M A</h3>
+								<p>C O N S U L T A<a href="Peticion-publica">Cómo Vamos?</a></p>
+							</div>
+						</div>
+					</li>
+					{{-- <li>
+						<div class="ch-item ch-img-3">
+							<div class="ch-info">
+								<h3>Pink Lightning</h3>
+								<p>by Charlie Wagers <a href="http://drbl.in/ekhp">View on Dribbble</a></p>
+							</div>
+						</div>
+					</li> --}}
+				</ul>
 
-				<div class="module form-module">
+				
 					
-				  <div class="toggle"><i class="fa fa-times fa-pencil"></i>
-				    H
-				  </div>
-				  <div class="form">
-				    <h2>Sistema de Atención</h2>
-				    <div class="btn-group">
-				   		 <a href="">
-						 	<button type="button" class="btn btn-orange btn-ripple">Ver video
-						 		<span class="ripple _2 animate" style="height: 322px; width: 322px; top: -138.087px; left: -74.7848px;"></span>
-						 		<span class="ripple _7 animate" style="height: 322px; width: 322px; top: -137.087px; left: 38.2152px;"></span></button>
-				  		</a>
-						 <a href="Peticion-publica">
-						 	<button type="button" class="btn btn-orange btn-ripple">Crear Peticion
-						 		<span class="ripple _2 animate" style="height: 322px; width: 322px; top: -138.087px; left: -74.7848px;"></span>
-						 		<span class="ripple _7 animate" style="height: 322px; width: 322px; top: -137.087px; left: 38.2152px;"></span></button>
+{{-- 				  
+				   		 
+						<a href="Peticion-publica">
+						 	<img style="width:150px; height="150px" src="{{ asset('assets/globals/img/resources/requests.png') }}" />
+						 	
 				  		</a>
 				  		<a href="" class="item btn" id="open-petition-modal">
-						 	<button type="button" class="btn btn-orange btn-ripple">Estado de Peticion!
-						 		<span class="ripple _2 animate" style="height: 322px; width: 322px; top: -138.087px; left: -74.7848px;"></span>
-						 		<span class="ripple _7 animate" style="height: 322px; width: 322px; top: -137.087px; left: 38.2152px;"></span></button>
+						 	
 				  		</a>
 				  		<a href="">
-						 	<button type="button" class="btn btn-orange btn-ripple">ingresar
-						 		<span class="ripple _2 animate" style="height: 322px; width: 322px; top: -138.087px; left: -74.7848px;"></span>
-						 		<span class="ripple _7 animate" style="height: 322px; width: 322px; top: -137.087px; left: 38.2152px;"></span></button>
+						 	
 				  		</a>					    
-					</div>
-				  </div>
-
-				  <div class="cta">Nos Mueve y Nos Une</div>
+					
 				 
-				</div>
+
+				  <div class="cta">Nos Mueve y Nos Une</div> 
+				  --}}
+				
 			</div>
 			
 
